efa8f09573748e0fe3a885a3b906e93f804d4be6
chagama <sak987760@gmail.com>
2026-05-17 08:25:13 +0900

    add animation

--- mysite/test/views.py
+++ mysite/test/views.py
@@ -1,8 +1,16 @@
-import requests
-import re
-import random
 from django.shortcuts import render
+from .models import Ternal
+
 
 def home(request):
-    
-    return render(request, "test.html", {"msg":""})
+    return render(request, "test.html")
+
+
+def save_memo(request):
+
+    if request.method == "POST":
+        text = request.POST.get("text")
+
+        Ternal.objects.create(text=text)
+
+    return render(request, "test.html")
