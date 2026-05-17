112db13e4b3dc9c7887d70280c9428e6ae82a049
chagama <sak987760@gmail.com>
2026-05-17 09:05:21 +0900

    PostgreSQL設定追加

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
