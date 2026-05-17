305dd3bd2135765ae725f120b1761d0db69c22ea
chagama <sak987760@gmail.com>
2026-05-17 09:54:24 +0900

    add animation

--- mysite/test/views.py
+++ mysite/test/views.py
@@ -16,3 +16,14 @@ def save_memo(request):
         Ternal.objects.create(text=text)
 
     return render(request, "test.html")
+from django.shortcuts import render
+from .models import Ternal
+
+
+def home(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        Ternal.objects.create(text=text)
+    
+    items = Ternal.objects.all()
+    return render(request, "test.html", {"items": items})
