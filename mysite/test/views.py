a249d7ca1834f9a5b6d459a079fba5bb135a2f4f
chagama <sak987760@gmail.com>
2026-05-19 15:03:26 +0900

    add animation

--- mysite/test/views.py
+++ mysite/test/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
-from .models import Ternal
 from django.shortcuts import render, redirect
-
-from django.shortcuts import render
 from .models import Ternal
 
 
@@ -11,7 +7,7 @@ def home(request):
         text = request.POST.get("text")
         Ternal.objects.create(text=text)
         return redirect('/')
-    items = Ternal.objects.all().order_by('-id')
+    items = Ternal.objects.all().order_by('id')
     return render(request, "test.html", {"items": items})
 
 
@@ -19,9 +15,11 @@ def save_memo(request):
     if request.method == "POST":
         text = request.POST.get("text")
         Ternal.objects.create(text=text)
-    
-    items = Ternal.objects.all().order_by('-id')
+        return redirect('/')
+    items = Ternal.objects.all().order_by('id')
     return render(request, "test.html", {"items": items})
+
+
 def delete_all(request):
     Ternal.objects.all().delete()
-    return render(request, "test.html", {"items": []})
+    return redirect('/')
