24272ad7175c883f967821c13172bfca846a6769
chagama <sak987760@gmail.com>
2026-05-19 22:11:21 +0900

    add animation

--- mysite/test/static/test/script.js
+++ mysite/test/static/test/script.js
@@ -1,5 +1,5 @@
 const textarea = document.querySelector('textarea');
 textarea.addEventListener('input', function() {
     this.style.height = 'auto';
-    this.style.height = Math.min(this.scrollHeight, 90) + 'px';
+    this.style.height = Math.min(this.scrollHeight, 72) + 'px';
 });
