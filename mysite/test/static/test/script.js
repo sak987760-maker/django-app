834535702f0aaa87f8fec18bfef87cc06f343669
chagama <sak987760@gmail.com>
2026-05-22 15:46:51 +0900

    add animation

--- mysite/test/static/test/script.js
+++ mysite/test/static/test/script.js
@@ -1,13 +1,15 @@
 const textarea = document.querySelector('textarea');
+const hozon = document.querySelector('.hozon');
+
+hozon.disabled = true;  // 最初は無効
+
 textarea.addEventListener('input', function() {
     this.style.height = 'auto';
     this.style.height = Math.min(this.scrollHeight, 72) + 'px';
+    
+    if (this.value === '') {
+        hozon.disabled = true;
+    } else {
+        hozon.disabled = false;
+    }
 });
-const text = textarea.value;
-const hozon = document.querySelector('.hozon');
-if (text === '') {
-    hozon.disabled = true;
-}
-else {
-    hozon.disabled = false;
-};
