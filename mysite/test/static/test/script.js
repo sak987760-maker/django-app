009820315309b457ddd232739e8be0fc73aa69ff
chagama <sak987760@gmail.com>
2026-05-22 15:36:23 +0900

    add animation

--- mysite/test/static/test/script.js
+++ mysite/test/static/test/script.js
@@ -3,3 +3,8 @@ textarea.addEventListener('input', function() {
     this.style.height = 'auto';
     this.style.height = Math.min(this.scrollHeight, 72) + 'px';
 });
+const text = textarea.value;
+const hozon = document.querySelector('.hozon');
+if (text === '') {
+    hozon.disabled = true;
+};
